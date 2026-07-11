f84d9ce436188b81afb0afabe9fb04941ecbcd75
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2026-07-11 14:45:41 +0200

    reading book

--- agent.py
+++ agent.py
@@ -2,21 +2,32 @@ import asyncio
 from ollama import AsyncClient
 from pathlib import Path
 
-pathlist = Path('definitions').glob('**/*.txt')
-for path in pathlist:
-    # because path is object not string
-    path_in_str = str(path)
-    print(path_in_str)
-    file = open(path_in_str, "r")
+testdata = ""
 
-    # Read the entire content of the file
-    content = file.read()
-    print(content)
+def read_definitions():
+    for path in Path(__file__).parent.glob('definitions/*.txt'):
+        path_in_str = str(path)
+        file = open(path_in_str, 'r')
+        content = file.read()
+        #print(content)
+        file.close()
 
+def read_test_data():
+    global testdata
+    for path in Path(__file__).parent.glob('TEST_DATA/*.txt'):
+        path_in_str = str(path)
+        print(path_in_str)
+        file = open(path_in_str, 'r', encoding='utf-8')
+        testdata = file.read()
+        #print(testdata)
+        file.close()
 
 async def chat():
-  message = {'role': 'user', 'content': 'Why is the sky blue?'}
-  response = await AsyncClient().chat(model='ibm/granite4:latest', messages=[message])
+  global testdata
+  message = {'role': 'user', 'content': 'Read following content and tell me summary in 5 short points. Do not explain what you do just print summary points. Content: '+ str(testdata) }
+  response = await AsyncClient().chat(model='granite4.1:3b', messages=[message])
   print(response)
 
-#asyncio.run(chat())
+#read_definitions()
+read_test_data()
+asyncio.run(chat())
