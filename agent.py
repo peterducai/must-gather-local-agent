68b278c4c1bcaad77f22e1b6cafd0647c06a1cdd
peter ducai <peter.ducai@gmail.com>
2026-08-18 11:19:11 +0200

    lanchain test

--- agent.py
+++ agent.py
@@ -1,20 +1,39 @@
 import asyncio
-from ollama import AsyncClient
+from ollama import Client
 from pathlib import Path
+import sys
 
-testdata = ""
+definitions = None
+testdata = None
+data_path = sys.argv[1]
+client = Client(
+  host='http://localhost:11434',
+  headers={'x-some-header': 'some-value'}
+)
 
 def read_definitions():
+    global definitions
     for path in Path(__file__).parent.glob('definitions/*.txt'):
         path_in_str = str(path)
         file = open(path_in_str, 'r')
-        content = file.read()
+        definitions = file.read()
         #print(content)
         file.close()
 
-def read_test_data():
+def collect_logs():
+    for path in Path(__file__).parent.glob('logs/*.txt'):
+        path_in_str = str(path)
+        print(path_in_str)
+        file = open(path_in_str, 'r', encoding='utf-8')
+        testdata = file.read()
+        # print(testdata)
+        file.close()
+
+def read_data_folder():
     global testdata
-    for path in Path(__file__).parent.glob('TEST_DATA/*.txt'):
+    global data_path
+    print(data_path)
+    for path in Path(__file__).parent.glob(data_path):
         path_in_str = str(path)
         print(path_in_str)
         file = open(path_in_str, 'r', encoding='utf-8')
@@ -22,12 +41,20 @@ def read_test_data():
         #print(testdata)
         file.close()
 
-async def chat():
+def chat():
   global testdata
-  message = {'role': 'user', 'content': 'Read following content and tell me summary in 5 short points. Do not explain what you do just print summary points. Content: '+ str(testdata) }
-  response = await AsyncClient().chat(model='granite4.1:3b', messages=[message])
+  global definitions
+  message = {'role': 'user', 'content': definitions }
+  response = client.chat(model='granite4.1:3b', messages=[message])
+  print(response)
+
+def chat2():
+  global testdata
+  message = {'role': 'user', 'content': 'Check log for any errors or warnings. Content: '+ str(testdata) }
+  response = client.chat(model='granite4.1:3b', messages=[message])
   print(response)
 
 #read_definitions()
-read_test_data()
-asyncio.run(chat())
+read_data_folder()
+chat()
+chat2()
